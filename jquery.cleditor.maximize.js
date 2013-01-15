fee4581611680eb7ce93e7287b2d91615d624beb
Milan Svoboda <msvoboda@ra.rockwell.com>
2013-01-15 07:59:30 +0100

    remember/set z-index, formatting retab

--- jquery.cleditor.maximize.js
+++ jquery.cleditor.maximize.js
@@ -2,7 +2,7 @@
 //updated and fixed by Milan Svoboda <milan.svoboda@centrum.cz>
 
 var clmai18n_en = {
-    title: "Maximize"
+	title: "Maximize"
 };
 
 if (!clmai18n) {
@@ -10,63 +10,67 @@ if (!clmai18n) {
 }
 
 (function($) {
-$.cleditor.buttons.maximize = {
-	name: "maximize",
-  	image: "maximize.gif",
+	$.cleditor.buttons.maximize = {
+		name: "maximize",
+	image: "maximize.gif",
 	title: clmai18n.title,
 	command: "",
 	popupName: "",
 	buttonClick: maximizeButtonClick
-};
+	};
 
-$.cleditor.defaultOptions.controls =
-$.cleditor.defaultOptions.controls.replace(" source", " source maximize");
+	$.cleditor.defaultOptions.controls =
+	$.cleditor.defaultOptions.controls.replace(" source", " source maximize");
 
 $.cleditor.defaultOptions.maximized = false;
 
 function maximizeButtonClick(e, data) {
 	var editor = data.editor;
-    var b = $("body");
-    var m = editor.$main;
+	var b = $("body");
+	var m = editor.$main;
 
 	if (editor.options.maximized == false) {
-        editor.options.orig = {
-            overflow: b.css("overflow"),
-            position: m.css("position"),
-            width: m.css("width"),
-            height: m.css("height"),
-            offset: m.offset()
-        };
+		editor.options.orig = {
+			overflow: b.css("overflow"),
+			position: m.css("position"),
+			width: m.css("width"),
+			height: m.css("height"),
+			offset: m.offset(),
+			zindex: m.css("z-index")
+		};
 
-		b.css({ overflow: "hidden" })
+		b.css({ overflow: "hidden" });
 		m.css({ position: "absolute", top: 0, left: 0,
-                width: $(window).width() - 4,
-                height: $(window).height() - 4 });
+			width: $(window).width() - 4,
+			height: $(window).height() - 4 });
 		m.width($(window).width() - 4);
 		m.height($(window).height() - 4);
 		m.offset({ top: 0, left: 0 });
+		m.css({ 'z-index': 1 });
 
 		editor.options.maximized = true;
-		editor.options.scrollto = 0
+		editor.options.scrollto = 0;
 	} else {
-        var orig = editor.options.orig;
+		var orig = editor.options.orig;
 
-		b.css({ overflow: orig.overflow })
+		b.css({ overflow: orig.overflow });
 		m.css({ position: orig.position, top: orig.offset.top, left: orig.offset.left,
-                width: orig.width,
-                height: orig.height });
+			width: orig.width,
+			height: orig.height });
 		m.width(orig.width);
 		m.height(orig.height);
 		m.offset(orig.offset);
- 
+		m.css({ 'z-index': orig.zindex });
+
 		editor.options.maximized = false;
-		editor.options.scrollto = m.position().top
+		editor.options.scrollto = m.position().top;
 	}
 
 	editor.updateTextArea();
 	editor.refresh();
-	$(window).scrollTop(editor.options.scrollto)
+	$(window).scrollTop(editor.options.scrollto);
 	if (!$.browser.msie) { editor.focus(); } // don't run with scrollTo in IE
+
 	return false;
 }
 })(jQuery);
